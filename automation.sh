24f2ba2b2960a86ec4575dd4f2200ddd6b0dbc13
Matias Ceau <matias@ceau.net>
2024-11-13 02:25:43 +0100

    feat: Add automation script, update .gitignore
    - Update .gitignore to exclude files ending with ".lockautomation.sh".
    - Add automation script for managing repositories and tools with argc commands.

---
+++ automation.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+cd "$GIT_REPOS/FORKS/llm-functions" || echo "no forks on this machine"
+argc link-web-search web_search_aichat.sh
+argc list@tool >> tools.txt
+argc list@agent >> agents.txt
+functions_path="$(aichat --info | rg '^functions' | awk '{print $2}')"
+[ -s "$functions_path" ] && echo "found function symlinks"
+rm "$functions_path"
+argc build
+argc install
